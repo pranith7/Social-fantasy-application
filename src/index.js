ccbe46478237889471f7b3d07aebdf22f60741e0
Pranith Pashikanti <pashikantipranith7867@gmail.com>
2024-06-17 17:46:41 +0000

    utils

--- src/index.js
+++ src/index.js
@@ -11,39 +11,15 @@ dotenv.config({
 })
 
 connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-( async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error) => {
-            console.error("Error: ", error)
-            throw error
-        })  
-        app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
-        })      
-    } catch (error) {
-        console.error("Error: ", error)
-        throw err
-    }
-})()
-
-*/
+.then(() => {
+    app.on(process.env.PORT, (error) => {
+        console.log("Err: ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at :${process.env.PORT}`);
+       })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed !!!", err);
+})
